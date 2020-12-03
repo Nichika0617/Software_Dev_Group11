71ae1da897cffb509587a6cfc0cb6f8de506af67
Yuge Nichika <e195765@ie.u-ryukyu.ac.jp>
2020-12-03 15:46:07 +0900

    add label

--- TodoList.py
+++ TodoList.py
@@ -5,4 +5,6 @@ root = tk.Tk()                # 窓を作る
 root.title("ToDoリスト")   # 窓のタイトルを設定
 root.geometry("640x480")      # 窓の大きさを設定
 
+label = tk.Label(root, text="\n \n やっとtkinterが動きました")
+label.pack()
 root.mainloop()
