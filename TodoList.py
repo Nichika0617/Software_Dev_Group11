59141e3175608b37072b0748519f38641e9fa0f1
由解　仁知翔 <e195765@ie.u-ryukyu.ac.jp>
2021-01-28 10:49:46 +0900

    マージ

--- TodoList.py
+++ TodoList.py
@@ -3,42 +3,68 @@ from tkinter import messagebox as tkMessageBox
 from tkinter import font
 import sys
 import tkinter
-import datetime 
+import datetime
+dt_now = datetime.datetime.now() #日付
 from functools import partial #ボタンコマンドに引数を渡す
 import pickle #プログラムを実行し終えたあとも作成したオブジェクトを保存するモジュール．
 import os
 from collections import OrderedDict
+import cronpi #定時にpush通知を送信するプログラムを実行
+
 
 os.chdir(os.path.dirname(__file__))#画像ファイルと同じディレクトリへ
 
-#処理はこの間に書く．
 #新規ウィンドウ作成
 root = tkinter.Tk()
+root.title(u"todo_app")
+root.geometry("800x450")
+
+#main_frameの設定
+main_frame = tkinter.Frame(width=800,height=450)
+main_frame.grid(row=0, column=0, sticky="nsew")#frameを重ねる
+
+#背景画像
+#ファイルを参照．ppmファイルなら表示可能
+main_back = tkinter.PhotoImage(file="cat.ppm")
+#画像の大きさを調整
+canvas = tkinter.Canvas(main_frame,bg="black", width=800, height=450)
+#画像の位置を設定
+canvas.place(x=0, y=0)
+#画像を表示する
+canvas.create_image(0, 0, image=main_back, anchor=tkinter.NW)
+
+changePageButton = tkinter.Button(main_frame, text="ToDoリスト",width=10,height=5, command=lambda : changePage(frame1))
+changePageButton.place(x=350,y=350)
+
+#frame1の設定
+frame1 = tkinter.Frame(width=800,height=450)
+frame1.grid(row=0, column=0, sticky="nsew")
+frame1.propagate(0)
+changePageButton = tkinter.Button(frame1, text="back", command=lambda : changePage(main_frame))
+changePageButton.place(x=740,y=0)
+
+
 #背景画像
 #ファイルを参照．ppmファイルなら表示可能
 back = tkinter.PhotoImage(file="watercolor.ppm")
 #画像の大きさを調整
-canvas = tkinter.Canvas(bg="black", width=400, height=400)
+canvas = tkinter.Canvas(frame1,bg="black", width=400, height=400)
 #画像の位置を設定
 canvas.place(x=0, y=0)
 #画像を表示する
 canvas.create_image(0, 0, image=back, anchor=tkinter.NW)
 
-#ウィンドウの名前を設定
-root.title(u"todo_app")
-#ウィンドウの大きさを設定
-root.geometry("800x450")
+
 staticfont = font.Font(family="Helvetica",size=20,weight="bold",) #フォント作成
-dt_now = datetime.datetime.now() #日付
 month = dt_now.month
-static = tkinter.Label(root,text=str(month)+"月のToDoリスト",font = staticfont,foreground='#6595ed') #起動した月を表示
+static = tkinter.Label(frame1,text=str(month)+"月のToDoリスト",font = staticfont,foreground='#6595ed') #起動した月を表示
 #年year、月month、日day、時間hour、分minute、秒second、マイクロ秒microsecondを整数intで取得できる。dt_now.dayなど．
 static.place(x=120, y=10)
-textBox = tkinter.Label(root,text="ToDoタスクを入力↓↓",font = staticfont,foreground='#ffa500')
+textBox = tkinter.Label(frame1,text="ToDoタスクを入力↓↓",font = staticfont,foreground='#ffa500')
 textBox.place(x=500, y=10)
 Explanation = "入力\n追加したいタスクを入力して\n[タスクを追加]ボタンをクリックすると，\nToDoリストに上から順番に追加されます．\n\n削除\n消したいタスクの番号のみを入力して，\n[タスクの削除]ボタンをクリックすると\nその番号のタスクを消すことができます．"
 Explanation_font = font.Font(family="Helvetica",size=15,weight="bold") #フォント作成
-useExplanation = tkinter.Label(root,text=Explanation,font = Explanation_font,foreground='#66cdaa')
+useExplanation = tkinter.Label(frame1,text=Explanation,font = Explanation_font,foreground='#66cdaa')
 useExplanation.place(x=450, y=120)
 
 #番号をkey,toDoをvalueとして辞書化　→　{0:toDo1, 1: toDo2, 2:toDo3, 3:toDo4, 4:toDo, 5:toDo, 6:toDo} #7個の番号はtask_entry[i]に対応させている
@@ -107,11 +133,14 @@ def check(event):
     tkMessageBox.showinfo('タスク状況',text)
 
 def QuitApp():
+    root.destroy()
+
+def store():
     with open('todo.pickle', 'wb') as f: #w書き込みモード,bバイナリモード
         pickle.dump(toDo_dic,f)
     with open('date.pickle', 'wb') as f: #w書き込みモード,bバイナリモード
         pickle.dump(date_dic,f)
-    root.destroy()
+    tkMessageBox.showinfo('保存完了',"ファイル「todo.pickle」,「date.pickle」にタスクを保存しました．")    
 
 def LoadData():
     with open('todo.pickle', 'rb') as f: #リード，バイナリ
@@ -131,6 +160,7 @@ Pythonでは、変数のスコープが代入の有無で変わる
 関数内で代入されていない：変数はグローバルスコープに決定
 らしい。。
 """
+
 def dateSort():
     i = 0
     sorted_toDo_dic =  {0:"", 1:"", 2:"", 3:"", 4: "", 5:"", 6:""}
@@ -146,66 +176,71 @@ def dateSort():
         task_entry[i].delete("0","end")
         task_entry[i].insert(tkinter.END,toDo_dic[i]) #task_entryへtoDoを出力
         task_entry[i].configure(state="readonly")
+    tkMessageBox.showinfo('完了！',"入力されたタスクを日付順に並び替えました．")
+
+def changePage(page):
+    '''
+    画面遷移用の関数
+    '''
+    page.tkraise()
 
 #チェックボックス
-box={}
-CheckBox = {}
-Val = {}
+box,CheckBox,Val={},{},{}
 for i in range(toDoNum):
     Val[i] = tkinter.BooleanVar()
     #Val[i].set(False)
-    CheckBox[i] = tkinter.Checkbutton(variable=Val[i])
+    CheckBox[i] = tkinter.Checkbutton(frame1,variable=Val[i])
     CheckBox[i].place(x=20,y=50+(i*40))
-    box[i] = tkinter.Label(root,text=str(i+1))
+    box[i] = tkinter.Label(frame1,text=str(i+1))
     box[i].place(x=0,y=50+(i*40))
-"""
-元のやつ
-Val1 = tkinter.BooleanVar()
-Val2 = tkinter.BooleanVar()
-CheckBox1 = tkinter.Checkbutton(variable=Val1)
-CheckBox1.place(x=10, y=50)
-box1 = tkinter.Label(root,text="1")
-box1.place(x=0, y=50)
-CheckBox2 = tkinter.Checkbutton(variable=Val2)
-CheckBox2.place(x=10, y=90)
-box2 = tkinter.Label(root,text="2")
-box2.place(x=0, y=90)
-"""
+
 #タスク確認ボタン
-button1 = tkinter.Button(text="タスク確認",width=30)
+button1 = tkinter.Button(frame1,text="タスク確認",width=30)
 button1.bind("<Button-1>",check)
 button1.place(x=60, y=350)
 
+
 #タスク追加ボタン
-add_buttom = tkinter.Button(text="タスクを追加",width=15,command=AddTask)
-add_buttom.config(bg="#F0F8FF")
+add_buttom = tkinter.Button(frame1,text="タスクを追加",width=15,command=lambda : AddTask())
 add_buttom.place(x=630, y=350)
 
 #タスク削除ボタン
 #delete_buttom = tkinter.Button(root,text="タスクを削除",width=15,command=partial(AddTask,-1))
-delete_buttom = tkinter.Button(text="タスクを削除",width=10,command=DeleteTask)
+delete_buttom = tkinter.Button(frame1,text="タスクを削除",width=10,command=lambda : DeleteTask())
 delete_buttom.place(x=420, y=350)
 
-#閉じるボタン
-quit_buttom = tkinter.Button(text="終了",width=10,command=QuitApp)
-quit_buttom.place(x=680, y=420)
+#保存ボタン
+store_buttom = tkinter.Button(frame1,text="保存",width=10,command=lambda : store())
+store_buttom.place(x=680, y=420)
+
+#終了ボタン
+quit_buttom = tkinter.Button(frame1,text="終了",width=10,command=lambda : QuitApp())
+quit_buttom.place(x=550, y=420)
 
 #ロードボタン
-load_buttom = tkinter.Button(text="ロード",width=10,command=LoadData)
+load_buttom = tkinter.Button(frame1,text="ロード",width=10,command=lambda : LoadData())
 load_buttom.place(x=420, y=420)
 
 #ソートボタン
-sort_buttom = tkinter.Button(text="日付順に並び替え",width=30,command=dateSort)
+sort_buttom = tkinter.Button(frame1,text="日付順に並び替え",width=30,command=lambda : dateSort())
 sort_buttom.place(x=60, y=420)
 
+#################################################################
+time = "10:49am"  #ここを変更して通知時間を指定する． 下のパスはそれぞれに合わせて．
+#################################################################
+cronpi.run_every_day("/usr/local/bin/python3.9 /Users/e195765/Desktop/2年講義/ソフトウェア開発演習/group11/Software_Dev_Group11/push.py").on(time)
+print("タスク終了1日前の{}に通知を送信します．".format(time))
+
+
 # エントリー(右側のテキストボックス)
-textBox = tkinter.Entry(width=40)
+textBox = tkinter.Entry(frame1,width=40)
 textBox.place(x=420, y=50)
 textBox.insert(tkinter.END,"ここにタスクを入力して，[追加]ボタンでタスクを追加")
 # エントリー(期限入力)
-task_deadline = tkinter.Entry(width=30)
+task_deadline = tkinter.Entry(frame1,width=30)
 task_deadline.place(x=480, y=80)
 task_deadline.insert(tkinter.END,"ここに期限を入力  例(1/7日なら'7'と入力)")
+
 """
 print("追加したいタスクを入力してください") #ターミナルで日本語入力
 inText = input()
@@ -215,7 +250,7 @@ textBox.insert(0,inText) #0文字目から
 #タスクを表示する用のエントリー(左側の閲覧用テキストボックス)
 task_entry = {}
 for i in range(toDoNum):
-    task_entry[i] = tkinter.Entry(width=35)
+    task_entry[i] = tkinter.Entry(frame1,width=35,)
     task_entry[i].place(x=50, y=50+(i*40))
     task_entry[i].configure(state='readonly')
 """
@@ -229,5 +264,7 @@ task2_entry.configure(state='readonly')
 """
 ################################################
 
+main_frame.tkraise()#最初に出てくるframeを設定
 #メインループ
 root.mainloop()
+
---
+++ push.py
@@ -0,0 +1,39 @@
+#push通知を送信するためのプログラム
+import pickle #プログラムを実行し終えたあとも作成したオブジェクトを保存するモジュール．
+import datetime 
+import os
+
+"""
+ AppleScliptを実行するためのコマンド．
+ 何も指定しなければスクリプトエディタからの通知となってしまうが，with title　でタイトルを指定できる．
+"""
+os.chdir(os.path.dirname(__file__))
+dt_now = datetime.datetime.now() #日付
+now_day = dt_now.day
+#年year、月month、日day、時間hour、分minute、秒second、マイクロ秒microsecondを整数intで取得できる。dt_now.dayなど．
+toDo_dic = {0:"", 1:"", 2:"", 3:"", 4: "", 5:"", 6:""} #初期valueは空白
+date_dic = {0:31, 1:31, 2:31, 3:31, 4:31, 5:31, 6:31} #初期valueは31日
+toDoNum = 7 #追加できるtoDoの数,task_entryの数に対応
+with open('todo.pickle', 'rb') as f: #リード，バイナリ
+    load_toDo_dic = pickle.load(f)
+with open('date.pickle', 'rb') as f: #リード，バイナリ
+    load_date_dic = pickle.load(f)
+for i in range(toDoNum):
+    toDo_dic[i] = load_toDo_dic[i]
+    date_dic[i] = load_date_dic[i]
+
+#print(date_dic) #{0: 9, 1: 11, 2: 12, 3: 31, 4: 31, 5: 31, 6: 31}
+#print(toDo_dic) #{0: '1/9 :  OS 12', 1: '1/11 :  OS11', 2: '1/12 :  OS ', 3: '', 4: '', 5: '', 6: ''}
+pushText = "以下のタスクの期限まで，残り1日です．\n"
+#print("今日は{}日です．".format(now_day))
+for i in range(toDoNum):
+    input_day = date_dic[i]
+    task = toDo_dic[i] #1/9 :  OS 12 keyが0,1,2なのでそのまま対応したvalueを取り出す．
+    print(task)
+    diff_Deadline = input_day - now_day #入力された期限と実際の期限の差
+    #print(diff_Deadline)
+    if diff_Deadline == 1: #1日前に通知を送る．
+        pushText += task + "\n"
+    
+os.system("osascript -e 'display notification \"{}\" with title \"todo.app\"'".format(pushText))
+#通知を送信．
